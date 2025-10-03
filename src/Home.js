3d98053ae9ef8fc4b4de331691ac6d75513520dd
AyBee <aybee076@gmail.com>
2025-10-03 12:10:44 +0000

    Version 1.15

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList'
+
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: 'My wbesite', body: 'lorem ipsum.....', author: 'mario', id: 1 },
+        {title: 'Welcome party!', body: 'lorem ipsum.....', author: 'yoshi', id: 2 },
+        {title: 'Web dev top tips', body: 'lorem ipsum....', author: 'luigi', id: 3 }
+    ]);
+
+    const [name, setName] = useState('mario');
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() => {
+        console.log('use efect ran');
+        console.log(name);
+    }, [name]);
+
+    return (
+        <div className="home">
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            <button onClick={() => setName('luigi')}> change button </button>
+            <p>{ name }</p>
+        </div>
+    )
+   
+}
+ 
+export default Home ;
