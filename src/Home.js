d9350fbe1d9bb75e3d4eb10ad8f38f1dea0297ad
AyBee <aybee076@gmail.com>
2025-10-04 08:33:45 +0000

    Version 1.18

--- src/Home.js
+++ src/Home.js
@@ -3,28 +3,33 @@ import BlogList from './BlogList'
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true)
 
     const [name, setName] = useState('mario');
 
    
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+       setTimeout(() => {
+         fetch('http://localhost:8000/blogs')
         .then(res =>{
             return res.json();
         })
         .then(data =>{
             setBlogs(data)
+            setIsPending(false)
         })
+       }, 1000)
     }, []);
 
     return (
         <div className="home">
+            {isPending && <div> Loading....</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"  /> }
             <button onClick={() => setName('luigi')}> change button </button>
             <p>{ name }</p>
         </div>
-    )
+        )
    
 }
  
