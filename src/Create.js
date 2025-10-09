c64e1d624d328160555a8c0a8dba9c2a5301cfe6
AyBee <aybee076@gmail.com>
2025-10-09 16:50:46 +0000

    Version 1.29

--- src/Create.js
+++ src/Create.js
@@ -2,14 +2,24 @@ import { useState } from "react";
 
 const Create = () => {
     const [title, setTitle] = useState('');
-    const [body, setBody] = useState('')
-    const [author, setAuthor] = useState('mario')
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false)
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = { title, body, author };
 
-        console.log(blog)
+        setIsPending(true)
+
+       fetch('http://localhost:8000/blogs', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(blog)
+       }).then(() => {
+        console.log('new blog added')
+        setIsPending(false)
+       })
     }
     return ( 
         <div className="create">
@@ -35,10 +45,8 @@ const Create = () => {
                 <option value="mario">mario</option>
                 <option value="yoshi">yoshi</option>
                </select>
-               <button>Add Blog</button>
-               {/* <h1>{title}</h1>
-               <p>{body}</p>
-               <h3>{author}</h3> */}
+               {isPending && <button>Adding Blog...</button>}
+               {!isPending && <button>Add Blog</button>}
             </form>
         </div>
      );
